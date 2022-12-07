4c3c0db7e05576af02c9f344f291a1819dd47382
yusragcelikel <yusragcelikel@gmail.com>
2022-12-07 10:17:09 +0300

    update

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,23 +1,39 @@
 from tkinter import *
 import cv2
-import mediapipe as mp
+#import mediapipe as mp
+
+print("Press 1 to SAVE process.")
+print("Press 2 to RECOGNIZE process.")
+process_chose = input()
 
 
-capture = cv2.VideoCapture(2)
-
-#cv2.namedWindow("Capture Image Through Webcam")
-
-
-import cv2
 
 cap = cv2.VideoCapture(0) #video objectimizi oluşturduk
+print("press ESC to close")
+print("press SPACE to save image")
+img_counter = 0
+
 
 while True:
     success, img = cap.read()
 
+    if img_counter == 3:
+        exit()
 
     cv2.imshow("Image", img)  # kamera görüntüsü image adlı pencerede gosterilir
-    cv2.waitKey(1)
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        break
+    elif k%256 == 32:
+#        print("Please type the image name: ")
+#        img_name = input("Please type the image name: ")
+        imageFullName = ("imageName{}.png".format(img_counter))
+        cv2.imwrite(imageFullName, img)
+        print("image captured")
+        img_counter += 1
+
+#    cv2.waitKey(1)
 
 
 
